21e5a68b8bf1cb89ae7eb51f60e6b5f6cd53bf7f
cjwsstrm <cjwesstrom@gmail.com>
2017-09-07 21:29:19 +0000

    sorting students by name then age, descending.

---
+++ sorting.js
@@ -0,0 +1,24 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+
+var sorted = students.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return b.age - a.age;
+});
+
+console.log(sorted);
